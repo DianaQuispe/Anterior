20747cdf542a2be7a52c208f604f8d59edf968f8
DianaQuispe <danaalienmas@gmail.com>
2017-06-26 18:08:20 -0500

    Update anterior.js

--- anterior.js
+++ anterior.js
@@ -1,4 +1,4 @@
-
+var assert = require("assert");
 //ejercicio1
 function rectangulo(base, altura) {
   this.base = base;
@@ -6,10 +6,8 @@ function rectangulo(base, altura) {
   this.area = function() {
     return this.base * this.altura; };
   }
-  var es = new rectangulo(2,2);
 //ejercicio2
-
-function Mujer(nombre,dia,mes,nacimiento){
+  function ejercicio2(nombre,dia,mes,nacimiento){
    this.nombre = nombre;
    this.dia = dia;
    this.mes = mes;
@@ -22,15 +20,15 @@ function Mujer(nombre,dia,mes,nacimiento){
      var diaA = fecha.getDate();
 
     var edadSi = anioA -this.nacimiento;
-   if (mesA <= this.mes && diaA <= this.dia) {
+   if (mesA < this.mes) {
+     edadSi--;
+   } if (mesA == this.mes && diaA <= this.dia) {
      edadSi--;
    }
-     return "La edad actual de "+ this.nombre +" es: "+ edadSi;
+     return "La edad actual de "+ this.nombre +" es "+ edadSi;
    }
 }
-   var cosa = new Mujer("Esserit",30, 10,1996);
-
-//ejercicio 3
+//ejercicio3
 function Cosa(nombre, edad,pasatiempo) {
   this.nombre = nombre;
   this.edad = edad;
@@ -39,5 +37,22 @@ function Cosa(nombre, edad,pasatiempo) {
     return "Hola soy " + this.nombre + " tengo " + this.edad + " y me gusta " + this.pasatiempo;
   }
 }
-  var men = new Cosa("Su", 34, "comer");
-  
+
+  describe('Ejercicio 1', function(){
+  it('Prueba de  2 y 2 son 4', function(){
+    var es = new rectangulo(2,2);
+    assert.equal(4, es.area());
+  });
+    });
+    describe('Ejercicio 2', function(){
+    it('Prueba deLa edad actual de Su es 25', function(){
+    var cosa = new ejercicio2("Su",23,8 ,1991);
+      assert.equal("La edad actual de Su es 25", cosa.edad());
+  });
+    });
+    describe('Ejercicio 2', function(){
+    it('Prueba deLa edad actual de Su es 25', function(){
+      var men = new Cosa("Su", 34, "comer");
+    assert.equal("Hola soy Su tengo 34 y me gusta comer", men.mensaje());
+    });
+});
