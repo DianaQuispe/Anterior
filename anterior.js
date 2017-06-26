ccf482a8419e07a9bf2568fc133523043f517b7e
DianaQuispe <danaalienmas@gmail.com>
2017-06-26 10:58:25 -0500

    Objetos sin TDD

---
+++ anterior.js
@@ -0,0 +1,46 @@
+
+//ejercicio1
+function rectangulo(base, altura) {
+  this.base = base;
+  this.altura = altura;
+  this.area = function() {
+    return this.base * this.altura; };
+  }
+  var es = new rectangulo(2,2);
+console.log(es.area());
+
+//ejercicio2
+
+function Mujer(nombre,dia,mes,nacimiento){
+   this.nombre = nombre;
+   this.dia = dia;
+   this.mes = mes;
+   this.nacimiento = nacimiento;
+   this.edad = function() {
+
+     var fecha = new Date();
+     var anioA = fecha.getFullYear();
+     var mesA =fecha.getMonth()+1;
+     var diaA = fecha.getDate();
+
+    var edadSi = anioA -this.nacimiento;
+   if (mesA <= this.mes && diaA <= this.dia) {
+     edadSi--;
+   }
+     return "La edad actual de "+ this.nombre +" es: "+ edadSi;
+   }
+}
+   var cosa = new Mujer("Esserit",30, 10,1996);
+console.log(cosa.edad());
+
+//ejercicio 3
+function Cosa(nombre, edad,pasatiempo) {
+  this.nombre = nombre;
+  this.edad = edad;
+  this.pasatiempo = pasatiempo;
+  this.mensaje = function(){
+    return "Hola soy " + this.nombre + " tengo " + this.edad + " y me gusta " + this.pasatiempo;
+  }
+}
+  var men = new Cosa("Su", 34, "comer");
+  console.log(men.mensaje());
